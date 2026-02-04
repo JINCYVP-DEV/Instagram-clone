d244f87910a796270f3d970dd4934e291e6aea54
jincy <jincy.vp.sk@gmail.com>
2026-02-04 09:36:13 +0530

    In progess : Setting Screen UI

--- app/src/main/java/com/nj/instagram/ui/auth/SettingsScreen.kt
+++ app/src/main/java/com/nj/instagram/ui/auth/SettingsScreen.kt
@@ -1,10 +1,27 @@
 package com.nj.instagram.ui.auth
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.hilt.navigation.compose.hiltViewModel
 
+sealed class SettingsScreenNavigationEvents{
+    object NavigateToLogin: SettingsScreenNavigationEvents()
+}
 @Composable
-fun SettingsScreen(viewModel: AuthViewModel= hiltViewModel())
+fun SettingsScreen(authViewModel: AuthViewModel= hiltViewModel(),onNavigation:(SettingsScreenNavigationEvents)->Unit)
 {
-
+    var showLogout by rememberSaveable { mutableStateOf(false) }
+    //observe authViewmodel state
+    LaunchedEffect(authViewModel.authEvents) {
+        authViewModel.authEvents.collect {event ->
+            if (event is AuthViewModel.AuthEvent.LoggedOut)
+            {
+                onNavigation(SettingsScreenNavigationEvents.NavigateToLogin)
+            }
+        }
+    }
 }
